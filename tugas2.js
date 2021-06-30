d4c52bdc3c2db021bc603a43c91273fd1fb7c316
oktyajipangestu <86592837+oktyajipangestu@users.noreply.github.com>
2021-06-30 21:07:48 +0700

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,13 @@
+let angka = 29;
+let kata = "hello";
+let logika = true;
+let kosong = null;
+let a = undefined;
+let list = [1, "hay", 5, true];
+
+console.log(angka);
+console.log(kata);
+console.log(logika);
+console.log(kosong);
+console.log(a);
+console.log(list);
